93a0dd1d581ff2f8e3685113ba1ce615f536c3a1
Javier Francisco Dibo Gomez <jfdg0002@red.ujaen.es>
2023-09-28 10:42:32 +0200

    Last commit?

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/jfdg0/Desktop/Informatica Grafcia y Visual/IGV-practica-02/pr2a_conan
+# Install script for directory: C:/Users/jfdg0/Desktop/Informatica Grafica y Visual/IGV-practica-02/pr2a_conan
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/jfdg0/Desktop/Informatica Grafcia y Visual/IGV-practica-02/pr2a_conan/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/jfdg0/Desktop/Informatica Grafica y Visual/IGV-practica-02/pr2a_conan/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
